62199e6c5ce02b0c927a145124c1b94f353394dc
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-01-27 20:40:57 -0800

    added basic elements to requests component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,28 @@ import { AppComponent } from './app.component';
 import { RequestsComponent } from './components/requests/requests.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { MatTabsModule} from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +36,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
