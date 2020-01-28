21d4346a1880491b96ff9fd4ec14e2915ca8cd10
robertlb4@gmail.com <robertlb4@gmail.com>
2020-01-27 21:02:47 -0800

    install and setup angular material and transloco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RequestsComponent } from './components/requests/requests.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { HttpClientModule } from '@angular/common/http';
-
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
-
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
-
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-
-import { MatTabsModule} from '@angular/material/tabs';
-import { MatListModule } from '@angular/material/list';
+import { TranslocoRootModule } from './transloco-root.module';
 import { RequestDetailComponent } from './components/request-detail/request-detail.component';
-import {MatIconModule} from '@angular/material/icon';
-
-
+import { RequestsComponent } from './components/requests/requests.component'
 
 
 @NgModule({
@@ -40,21 +19,9 @@ import {MatIconModule} from '@angular/material/icon';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatCardModule,
-    MatGridListModule,
-    MatToolbarModule,
-    MatTabsModule,
-    MatListModule,
-    MatIconModule
+    HttpClientModule,
+    TranslocoRootModule
   ],
   providers: [],
   bootstrap: [AppComponent]
