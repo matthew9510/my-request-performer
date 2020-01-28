f75a9cdba6cbdbc0e363e5ec66e4c50b8d1cab48
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-01-27 19:59:18 -0800

    created requests component & installed angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RequestsComponent } from './components/requests/requests.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'requests', component: RequestsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
