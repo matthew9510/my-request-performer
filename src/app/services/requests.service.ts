d28a54ab2bb4cacc63bb12aa1f47d77aaf763814
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-01-29 21:07:25 -0800

    added fetchRequests call on requestsService & subscribe method on Requests Component

--- src/app/services/requests.service.ts
+++ src/app/services/requests.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Requests } from '../interfaces/requests';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestsService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  fetchRequests() {
+    return this.http.get<Requests[]>('../assets/requests/requests.json');
+  }
+
 }
