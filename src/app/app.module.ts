e9191e57631d0515bbd821694f6311dcbc0a3160
Nathan Vu <nathan.vu5@gmail.com>
2020-01-29 21:47:15 -0800

    angular calender

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateEventComponent } from './create-event/create-event.component';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule
+ } from '@angular/material';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEventComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMomentDateModule,
+    MatDatepickerModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
