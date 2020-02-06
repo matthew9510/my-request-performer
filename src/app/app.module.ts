042a3fda1b2f501db36c6c1ff855be212ab76477
rhiannoncduvall@gmail.com <rhiannoncduvall@gmail.com>
2020-02-06 13:47:38 -0800

    merged develop branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, MatCheckboxModule, MatChipsModule, MatCardModule, MatGridListModule, MatTabsModule, MatListModule, MatIconModule, MatDialogModule } from '@angular/material';
+import { 
+  MatToolbarModule, 
+  MatFormFieldModule, 
+  MatInputModule, 
+  MatSelectModule, 
+  MatButtonModule, 
+  MatCheckboxModule, 
+  MatChipsModule, 
+  MatCardModule, 
+  MatGridListModule, 
+  MatTabsModule, 
+  MatListModule, 
+  MatIconModule, 
+  MatDialogModule 
+} from '@angular/material';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RequestsComponent } from './components/requests/requests.component';
 import { RequestDetailComponent } from './components/request-detail/request-detail.component';
 import { TranslocoRootModule } from './transloco-root.module';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
