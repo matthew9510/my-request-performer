d4b2491a9308e7bbab4ecbc5227824ee5d404e2a
robertlb4@gmail.com <robertlb4@gmail.com>
2020-02-10 21:23:25 -0800

    update form using angular reactive forms.  Additional styling required

--- src/app/components/create-event/create-event.component.ts
+++ src/app/components/create-event/create-event.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as moment from 'moment';
 
-export interface Time {
-  value: string;
-  viewValue: string;
-}
 
 @Component({
   selector: 'app-create-event',
@@ -13,44 +9,31 @@ export interface Time {
   styleUrls: ['./create-event.component.scss']
 })
 export class CreateEventComponent implements OnInit {
-  eventForm;
-  times: Time[] = [
-    { value: 'one-0', viewValue: 'zero' },
-    { value: 'two-1', viewValue: 'one' },
-    { value: 'three-2', viewValue: 'two' },
-    { value: 'four-3', viewValue: 'three' },
-    { value: 'five-4', viewValue: 'four' },
-    { value: 'seven-6', viewValue: 'five' }
-  ];
+  eventForm: FormGroup;
 
-  // times = [];
-
-  // constructor() {
-  //   // populate new list of times
-  //     this.createTimesWithOutReturn(); // if it doesn't return anything
-
-  //     this.times = this.createTimesWithReturn(); // if it does return something
-
-  // }
+  constructor() { }
 
   ngOnInit() {
-    console.log(moment().format());
-    this.eventForm = new FormGroup({});
+    this.eventForm = new FormBuilder().group({
+      title: [null, Validators.required],
+      date: [null, Validators.required],
+      startTime: [null, Validators.required],
+      endTime: [null, Validators.required],
+      coverFee: [null],
+      genre: [null, Validators.required],
+      venue: [null, Validators.required],
+      description: [null, Validators.required],
+      url: [null],
+      image: [null],
+    });
   }
 
-  // createTimesWithOutReturn(){
-  //   for(){
-  //     this.times.push()
-  //   }
-  //   // don't return
-  //   return void
-  // }
+  createEvent() {
+    console.log(this.eventForm.value);
+  }
 
-  // createTimesWithReturn(){
-  //   let tempArray = []
-  //   for (){
-  //     tempArray.push()
-  //   }
-  //   // don't return
-  //   return tempArray
+  imageUploaded(image) {
+    console.log(image.target.files[0])
+  }
+
 }
