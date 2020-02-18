a4ad22cd2a725233f34e1a7c45aa7307b4f53c5a
Nathan Vu <nathan.vu5@gmail.com>
2020-02-17 19:21:08 -0800

    subscribe to events and change components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { Requests } from './interfaces/requests';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RequestsComponent } from './components/requests/requests.component';
@@ -7,16 +8,18 @@ import { HistoryComponent } from './components/dashboard/history/history.compone
 import { ProfileComponent } from './components/dashboard/profile/profile.component';
 import { ManageEventsComponent } from './components/manage-events/manage-events.component';
 import { CreateEventComponent } from './components/create-event/create-event.component';
+import { HeaderComponent } from './components/header/header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'payout', component: PayoutComponent },
-  { path: 'history', component: HistoryComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'requests', component: RequestsComponent },
-  { path: 'events', component: ManageEventsComponent },
-  { path: 'create-event', component: CreateEventComponent }
+  { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } },
+  { path: 'payout', component: PayoutComponent, data: { title: 'Payout' } },
+  { path: 'history', component: HistoryComponent, data: { title: 'Histoy' } },
+  { path: 'profile', component: ProfileComponent, data: { title: 'Profile' } },
+  { path: 'requests', component: RequestsComponent, data: { title: 'Requests' } },
+  { path: 'events', component: ManageEventsComponent, data: { title: 'ManageEvents' } },
+  { path: 'create-event', component: CreateEventComponent, data: { title: 'create-event' } },
+  // { path: 'header', component: HeaderComponent}
 ];
 
 @NgModule({
