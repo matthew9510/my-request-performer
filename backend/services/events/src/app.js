e592e24b0306e8c8115623946b356d5cd695cd76
digital-underground <sd_pilot@hotmail.com>
2020-02-21 00:50:33 -0800

    Refactored endpoints to deploy single lambda containing CRUD commands

--- backend/services/events/src/app.js
+++ backend/services/events/src/app.js
@@ -6,93 +6,156 @@ or in the "license" file accompanying this file. This file is distributed on an 
 See the License for the specific language governing permissions and limitations under the License.
 */
 
-
-/* Amplify Params - DO NOT EDIT
-You can access the following resource attributes as environment variables from your Lambda function
-var environment = process.env.ENV
-var region = process.env.REGION
-var authMyrequest362310f3UserPoolId = process.env.AUTH_MYREQUEST362310F3_USERPOOLID
-
-Amplify Params - DO NOT EDIT */
-
-var express = require('express')
-var bodyParser = require('body-parser')
-var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const express = require('express');
+const bodyParser = require('body-parser');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
 
 // declare a new express app
-var app = express()
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next()
 });
 
+/**********************
+ *  Load AWS SDK for JavaScript
+ *  to interact with AWS DynamoDB*
+ **********************/
+
+const AWS = require("aws-sdk");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const table = process.env.DYNAMODB_TABLE;
+const datestamp = new Date();
+const datestamp = Date.parse(datestamp);
 
 /**********************
- * Example get method *
+ * GET method *
  **********************/
 
 app.get('/events', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
+  console.log("GET REQUEST...", req.body);
 
-app.get('/events/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
+  // create params
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: req.query.id,
+    },
+  };
+
+  // fetch event from the database
+  dynamoDb.get(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error("Unable to find item. Error JSON:", JSON.stringify(error, null, 2));
+    } else {
+      // create a response
+      const response = {
+        statusCode: 200,
+        body: result.Item,
+      };
+      res.json({success: 'Successfully found item in the events table!', response: response.body})
+    }
+  });
 });
 
 /****************************
-* Example post method *
-****************************/
-
-app.post('/events', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-app.post('/events/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example put method *
+* PUT method *
 ****************************/
 
 app.put('/events', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
+  console.log("PUT REQUEST...", req.body);
 
-app.put('/events/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  const params = {
+    TableName: table,
+    Item: req.body
+  };
+
+  params.Item.create_date = datestamp;
+
+  dynamoDb.put(params, function(err, result) {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      const response = {
+        statusCode: 200,
+        body: req.body,
+      };
+      res.json({success: 'Successfully added item to the events table!', record: response.body})
+    }
+  });
 });
 
 /****************************
-* Example delete method *
+* DELETE method *
 ****************************/
 
 app.delete('/events', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  console.log("DELETE EVENT REQUEST...", req.body);
+
+  // create params
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: req.query.id,
+    },
+  };
+
+  dynamoDb.delete(params, function(err, result) {
+    if (err) {
+      console.error("Unable to DELETE item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      const response = {
+        statusCode: 200,
+        body: req.body,
+      };
+      res.json({success: 'delete call for events table succeeded!', response: response});
+    }
+  });
 });
 
-app.delete('/events/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+/****************************
+ * PATCH method *
+ ****************************/
+
+app.patch('/events', function(req, res) {
+  console.log("UPDATE EVENT REQUEST...", req);
+
+  // create params
+  const params = {
+    TableName: table,
+    Key: {
+      id: req.query.id,
+    },
+    UpdateExpression: "set #n = :val1",
+    ExpressionAttributeValues:{":val1":req.query.name},
+    ExpressionAttributeNames:{"#n": "name"},
+    ReturnValues:"UPDATED_NEW"
+  };
+
+  dynamoDb.update(params, function(err, result) {
+    if (err) {
+      console.error("Unable to Update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      const response = {
+        statusCode: 200,
+        body: result,
+      };
+      res.json({success: 'UPDATE for record on events table succeeded!', response: response.body});
+    }
+  });
 });
 
 app.listen(3000, function() {
-    console.log("App started")
+    console.log("My Request API...")
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
+module.exports = app;
