2cb21873a3c7928cc7d4d87500dcd185e60c8e63
digital-underground <sd_pilot@hotmail.com>
2020-02-26 12:49:53 -0800

    Configuring the Amplify provider with Auth Amplify JS module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { EventdetailsComponent } from "./components/event-detail/event-detail.component";
-import { ManageEventsComponent } from "./components/manage-events/manage-events.component";
-import { RequestsComponent } from "./components/requests/requests.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatMomentDateModule } from "@angular/material-moment-adapter";
-import "hammerjs";
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
-import { PayoutComponent } from "./components/dashboard/payout/payout.component";
-import { HistoryComponent } from "./components/dashboard/history/history.component";
-import { ProfileComponent } from "./components/dashboard/profile/profile.component";
-import { HttpClientModule } from "@angular/common/http";
-import { ReactiveFormsModule } from "@angular/forms";
-import { RequestDetailComponent } from "./components/request-detail/request-detail.component";
-import { TranslocoRootModule } from "./transloco-root.module";
-import { ConfirmDialogComponent } from "./components/confirm-dialog/confirm-dialog.component";
-import { BottomNavComponent } from "./components/bottom-nav/bottom-nav.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventdetailsComponent } from './components/event-detail/event-detail.component';
+import { ManageEventsComponent } from './components/manage-events/manage-events.component';
+import { RequestsComponent } from './components/requests/requests.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import 'hammerjs';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PayoutComponent } from './components/dashboard/payout/payout.component';
+import { HistoryComponent } from './components/dashboard/history/history.component';
+import { ProfileComponent } from './components/dashboard/profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RequestDetailComponent } from './components/request-detail/request-detail.component';
+import { TranslocoRootModule } from './transloco-root.module';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
+import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
 import {
   MatToolbarModule,
   MatIconModule,
@@ -33,10 +33,17 @@ import {
   MatListModule,
   MatDialogModule,
   MatDatepickerModule
-} from "@angular/material";
+} from '@angular/material';
 
-import { ScrollingModule } from "@angular/cdk/scrolling";
-import { CreateEventComponent } from "./components/create-event/create-event.component";
+
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CreateEventComponent } from './components/create-event/create-event.component';
+
+
+// Configuring the Amplify provider with specified Amplify JS modules
+// https://aws-amplify.github.io/docs/js/angular#option-2-configuring-the-amplify-provider-with-specified-amplify-js-modules
+import { AmplifyAngularModule, AmplifyService, AmplifyModules } from 'aws-amplify-angular';
+import Auth from '@aws-amplify/auth';
 
 @NgModule({
   declarations: [
@@ -83,9 +90,19 @@ import { CreateEventComponent } from "./components/create-event/create-event.com
     TranslocoRootModule,
     ScrollingModule,
     MatDatepickerModule,
-    MatMomentDateModule
+    MatMomentDateModule,
+    AmplifyAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AmplifyService,
+      useFactory:  () => {
+        return AmplifyModules({
+          Auth
+        });
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
