69b478a3733a989c8a61952f85b80a99612f9fba
Peter Horton <phorton@softstackfactory.com>
2020-03-26 23:25:53 -0700

    added completed completed script for aws amplify and glip (#127)
    * added completed completed script for aws amplify and glip

---
+++ webhook.sh
@@ -0,0 +1,7 @@
+curl -X POST -H "Content-Type: application/json" -d '
+ {
+  "activity":"AWS AMPLIFY",
+  "title":"Frontend Build Successful",
+  "icon":"https://static.skillshare.com/uploads/project/239dbec8bcc75f80457b1caa47a4a657/a801f44f",
+  "body":"*  MY REQUEST: [The Funky Buddha Lounge](http://www.thefunkybuddha.com)"
+ }' https://hooks.glip.com/webhook/d8d7aeaa-a70d-4039-8988-83e213d9d8e2
