7833da7a4903918f949145c01c8c9fe131e8d620
Rhiannon Duvall <53802130+rhiannoncduvall@users.noreply.github.com>
2020-04-08 11:19:09 -0700

    requests show the number of requests/top ups for a song (#171)

--- src/app/components/request-detail/request-detail.component.ts
+++ src/app/components/request-detail/request-detail.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, OnInit, Input } from "@angular/core";
+import { BreakpointObserver } from "@angular/cdk/layout";
 
 @Component({
-  selector: 'app-request-detail',
-  templateUrl: './request-detail.component.html',
-  styleUrls: ['./request-detail.component.scss']
+  selector: "app-request-detail",
+  templateUrl: "./request-detail.component.html",
+  styleUrls: ["./request-detail.component.scss"],
 })
 export class RequestDetailComponent implements OnInit {
   @Input() artist: string;
@@ -13,20 +13,17 @@ export class RequestDetailComponent implements OnInit {
   @Input() memo: string;
   @Input() status: string;
   @Input() createdOn: string;
+  @Input() amountOfTopUps: number;
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   get isSmallScreen() {
-    return this.breakpointObserver.isMatched('(max-width: 450px)');
+    return this.breakpointObserver.isMatched("(max-width: 450px)");
   }
 
   get isLargeScreen() {
-    return this.breakpointObserver.isMatched('(min-width: 700px)')
+    return this.breakpointObserver.isMatched("(min-width: 700px)");
   }
-
-
 }
