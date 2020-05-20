c2eaa31e55c73d8515e21da2290a05be9f839a46
Matthew Hess <matthew9510@gmail.com>
2020-05-20 16:31:16 -0700

    Aws prod env (#281)
    * Implemented the profile page.
    Login redirects to profile if first time performer logs in
    Linked form
    Save performer details in database
    
    * setup cognito pool credentials for the production environment
    
    * Deleted comments

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,8 @@ import { AppModule } from "./app/app.module";
 import { environment } from "@ENV";
 
 import Amplify from "@aws-amplify/core";
-// import awsconfig from "./aws-exports";
-// Amplify.configure(awsconfig);
 
 Amplify.configure({
-  // OPTIONAL - if your API requires authentication
-  // Auth: {
-  //   // REQUIRED - Amazon Cognito Identity Pool ID
-  //   identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
-  //   // REQUIRED - Amazon Cognito Region
-  //   region: 'XX-XXXX-X',
-  //   // OPTIONAL - Amazon Cognito User Pool ID
-  //   userPoolId: 'XX-XXXX-X_abcd1234',
-  //   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-  //   userPoolWebClientId: 'a1b2c3d4e5f6g7h8i9j0k1l2m3',
-  // },
   API: {
     endpoints: [
       {
@@ -35,7 +22,6 @@ Amplify.configure({
     ],
   },
   aws_project_region: environment.aws_project_region,
-  aws_cognito_identity_pool_id: environment.aws_cognito_identity_pool_id,
   aws_cognito_region: environment.aws_cognito_region,
   aws_user_pools_id: environment.aws_user_pools_id,
   aws_user_pools_web_client_id: environment.aws_user_pools_web_client_id,
